62e68c25cc0a3cd08b3abcaa810a1aea624c6185
1197500583@qq.com <1197500583@qq.com>
2021-08-28 17:46:08 +0800

    dev

--- demo/src/main/java/dataSource/demo/DemoApplication.java
+++ demo/src/main/java/dataSource/demo/DemoApplication.java
@@ -2,12 +2,23 @@ package dataSource.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+
+	@RequestMapping("/demo")
+	public Boolean demo(){
+		return false;
 	}
 
 }
